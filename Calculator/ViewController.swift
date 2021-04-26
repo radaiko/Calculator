2b4f5c7b6eb9d6cc1b9e878e723ff5c480e81915
Aiko Radlingmayr <a.radlingmayr@icloud.com>
2021-04-26 21:32:01 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Aiko Radlingmayr on 26.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
